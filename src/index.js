56be29537ff236a76d71acaade395cd81d47a087
cmyk52 <j.lmarambio@hotmail.com>
2024-08-16 17:35:09 -0400

    Primer commit

---
+++ src/index.js
@@ -0,0 +1,49 @@
+import express from 'express'
+import dotenv from 'dotenv'
+import path from 'path'
+import { fileURLToPath } from 'url';
+
+const app = express()
+
+//.ENV
+dotenv.config()
+const PORT = process.env.PORT ?? 4000
+
+
+
+
+//RUTAS
+
+//Configurando directorios
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+
+//Index url
+app.get ('/', (req, res)=>{
+    console.log('index')
+    res.sendFile(path.join(__dirname, '../public/index.html'))
+})
+
+//Contacto url
+app.get('/contacto', (req,res)=>{
+    console.log('contacto')
+    res.sendFile(path.join(__dirname, '../public/contacto.html'))
+
+})
+
+//404 - Manejo de error al solicitar url
+app.use((req,res)=>{
+    console.log('contacto')
+    res.sendFile(path.join(__dirname, '../public/404.html'))
+})
+
+
+
+//Server PORT
+app.listen(PORT, ()=>{
+    console.log(`Servidor levantado en el puerto http://localhost:${PORT}`)
+})
+
+
+
